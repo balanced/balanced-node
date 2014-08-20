13fa624b05244e607cc9e25e1e1be1081cdc4d67
richie serna <richie@balancedpayments.com>
2014-08-20 14:48:33 -0700

    Edit credit-reverse

--- snippets/credit-reverse.js
+++ snippets/credit-reverse.js
@@ -1,4 +1,4 @@
-order.credits.get( 0 ).then(function( credit ) {
+balanced.get( credit_href ).then(function( credit ) {
     credit.reversal({
         "amount": 8000,
         "description": "Reversal for Order #1111"
