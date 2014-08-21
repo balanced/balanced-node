cc8381c8b07f48a1d786245ff35c58d129194585
Ben Mills <ben@unfiniti.com>
2014-08-21 10:08:18 -0600

    Use consistent quotation style

--- snippets/credit-marketplace-escrow.js
+++ snippets/credit-marketplace-escrow.js
@@ -1,4 +1,4 @@
 balanced.marketplace.owner_customer.bank_accounts.get( 0 ).credits.create({
     amount: 2000000,
-    "description": 'Credit from Balanced escrow'
+    "description": "Credit from Balanced escrow"
 })
