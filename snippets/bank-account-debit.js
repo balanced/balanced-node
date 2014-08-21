fe13ed4575efbe2acb8dd6b22b825e6dc8442894
richie serna <richie@balancedpayments.com>
2014-08-21 14:38:37 -0700

    Add order param to credit and debit scenarios

--- snippets/bank-account-debit.js
+++ snippets/bank-account-debit.js
@@ -1,6 +1,8 @@
-// bank_account_href is the stored href for the bank account
+// bank_account_href is the stored href for the BankAccount
+// order_href is the stored href for the Order
 balanced.get( bank_account_href ).debit({
     "appears_on_statement_as": "Statement text", 
     "amount": 5000, 
-    "description": "Some descriptive text for the debit in the dashboard"
+    "description": "Some descriptive text for the debit in the dashboard",
+    "order": order_href
 })
--- snippets/reversal-create.js
+++ snippets/reversal-create.js
@@ -1,4 +1,5 @@
-// credit_href is the stored href for the credit
+// credit_href is the stored href for the Credit
+// order_href is the stored href for the Order
 balanced.get( credit_href ).reversal({
     "amount": 3000, 
     "meta": {
@@ -6,5 +7,6 @@ balanced.get( credit_href ).reversal({
         "user.refund_reason": "not happy with product", 
         "fulfillment.item.condition": "OK"
     }, 
-    "description": "Reversal for Order #1111"
+    "description": "Reversal for Order #1111",
+    "order": order_href
 })
