fbea66326c9e5e60c220a150c8005dea5fe2597b
richie serna <richie@balancedpayments.com>
2014-09-26 16:37:13 -0700

    Edit order credit to marketplace

--- snippets/order-credit-marketplace.js
+++ snippets/order-credit-marketplace.js
@@ -1,4 +1,3 @@
-balanced.marketplace.owner_customer.bank_accounts.get( 0 ).credits.create({
-    amount: 2000,
-    "description": "Credit from order escrow to marketplace bank account"
+var marketplaceAccount = balanced.marketplace.owner_customer.bank_accounts.get( 0 ).then(function( marketplaceAccount ) {
+    order.(marketplaceAccount, 2000)
 })
